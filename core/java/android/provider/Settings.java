136c501463e006eacc31b72ec104a28da157ad34
cristianomatos <cristianobmatos@gmail.com>
2013-11-08 11:31:45 -0200

    Network stats: Hide if there's no traffic (1/2)
    Change-Id: I8b770c7ad803c063cf995e5f88c8f44e0eeae86a
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkStatsView.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2959,7 +2959,13 @@ public final class Settings {
          * Network stats Color style
          * @hide
          */
-        public static final String STATUS_BAR_NETWORK_COLOR = "status_bar_network_usage_color"; 	
+        public static final String STATUS_BAR_NETWORK_COLOR = "status_bar_network_usage_color";
+
+        /**
+         * Network stats hide if there's no traffic
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_HIDE = "status_bar_network_hide"; 	
 
         /**
          * Display style of the status bar battery information
