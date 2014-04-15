1da2329f1ffd21634e38c3789f4cabfe98f7c112
Lars Greiss <kufikugel@googlemail.com>
2014-04-15 09:04:26 -0300

    Frameworks: go kitkat + style tweaks and follow googles color sheme
    Googles color sheme on cards is
    
    card: ffffff
    aditional content: f5f5f5
    background: e5e5e5
    
    first two we did...background not. Follow the color sheme due it has a better contrast.
    
    As well remove the non google rounded edges.
    
    Allow drawing behin status and navigation bar if the bars are translucent
    
    PS:
    - due it is now a system window ensure that all other system windows are closed when opening recents.
    PS
    - minor tweaks
    PS
    - remove one unused flag
    PS
    - respect app system ui visibility status and reflect it for smoother experience
    PS
    - bring back TYPE_RECENT_OVERLAY window layouparams back to live. It was never documented
    nor depreciated. Was used in the past for GB recents and is even updated till today although
    nothing in the system is using it.
    
    Make it fully functionall (it is basically just an extended TYPE_SYSTEM_DIALOG) and allow
    this window type to extend its content behind navigation bar.
    
    PS
    - minor fix...init the systemUI flags for the view allready on preload to prevent positioning
    of the listview.
    PS
    - draw always behind navigation bar as long it is not immersive mode or hide navigation bar mode
    - add more margin to the delete icon for kill all
    
    Change-Id: If38036dded961fcf0a6d8c2e7d2ba44a8e1dd08a

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -10910,4 +10910,11 @@ public class WindowManagerService extends IWindowManager.Stub
     public void addSystemUIVisibilityFlag(int flag) {
         mLastStatusBarVisibility |= flag;
     }
+
+    /* @hide */
+    @Override
+    public int getSystemUIVisibility() {
+        return mLastStatusBarVisibility;
+    }
+
 }
