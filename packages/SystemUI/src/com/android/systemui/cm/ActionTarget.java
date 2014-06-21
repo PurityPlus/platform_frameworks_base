2ace2b7861a3076ba0b5b6458f29ca5b65874509
Jacob Mueller <jacob.mueller.elz@gmail.com>
2014-06-21 18:19:10 +0200

    frameworks/base: Update quick launch shortcut icons/resources
    Update the existing icons and add "xxhdpi" versions created
    from a vector template. This commit cleans also the resource
    files a bit up.
    
    Screenshot: http://goo.gl/6CrMYr
    
    Change-Id: I45abee5cf13cd239eb5b4402019afac28d6d5952
    
    Conflicts:
    
    	core/res/res/drawable-mdpi/ic_navigation_ring_blank_activated.png
    	core/res/res/drawable-mdpi/ic_navigation_ring_blank_normal.png
    	core/res/res/drawable-mdpi/ic_navigation_ring_ime_switcher_activated.png
    	core/res/res/drawable-mdpi/ic_navigation_ring_ime_switcher_normal.png
    	core/res/res/drawable-mdpi/ic_navigation_ring_screenshot_activated.png
    	core/res/res/drawable-mdpi/ic_navigation_ring_screenshot_normal.png
    	core/res/res/drawable-mdpi/ic_navigation_ring_silent_activated.png
    	core/res/res/drawable-mdpi/ic_navigation_ring_silent_normal.png
    	core/res/res/drawable-mdpi/ic_navigation_ring_sound_on_activated.png
    	core/res/res/drawable-mdpi/ic_navigation_ring_sound_on_normal.png
    	core/res/res/drawable-mdpi/ic_navigation_ring_vibrate_activated.png
    	core/res/res/drawable-mdpi/ic_navigation_ring_vibrate_normal.png
    	core/res/res/values/dimens.xml
    	core/res/res/values/symbols.xml

--- packages/SystemUI/src/com/android/systemui/cm/ActionTarget.java
+++ packages/SystemUI/src/com/android/systemui/cm/ActionTarget.java
@@ -1,6 +1,6 @@
 /*
+ * Copyright (C) 2013-2014 The CyanogenMod Project
  * Copyright (C) 2013 AOKP by Mike Wilson - Zaphod-Beeblebrox && Steve Spear - Stevespear426
- * Copyright (C) 2013 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,10 +57,9 @@ import com.android.systemui.statusbar.phone.KeyguardTouchDelegate;
 
 import java.net.URISyntaxException;
 
-/*
+/**
  * Helper classes for managing custom actions
  */
-
 public class ActionTarget {
     private static final String TAG = "ActionTarget";
 
@@ -92,8 +91,9 @@ public class ActionTarget {
                 dismissKeyguard();
                 getStatusBarService().toggleRecentApps();
             } catch (RemoteException e) {
-                // ignored
+                // Do nothing here
             }
+
             return true;
         } else if (action.equals(ACTION_HOME)) {
             injectKeyDelayed(KeyEvent.KEYCODE_HOME);
@@ -104,10 +104,10 @@ public class ActionTarget {
         } else if (action.equals(ACTION_MENU)) {
             injectKeyDelayed(KeyEvent.KEYCODE_MENU);
             return true;
-        } else if (action.equals(ACTION_POWER)) {
+        } else if (action.equals(ACTION_STANDBY)) {
             injectKeyDelayed(KeyEvent.KEYCODE_POWER);
             return true;
-        } else if (action.equals(ACTION_IME)) {
+        } else if (action.equals(ACTION_IME_SWITCHER)) {
             mContext.sendBroadcast(new Intent("android.settings.SHOW_INPUT_METHOD_PICKER"));
             return true;
         } else if (action.equals(ACTION_SCREENSHOT)) {
@@ -138,7 +138,7 @@ public class ActionTarget {
             }
 
             return true;
-        } else if (action.equals(ACTION_KILL)) {
+        } else if (action.equals(ACTION_KILL_TASK)) {
             mHandler.post(mKillRunnable);
             return true;
         } else if (action.equals(ACTION_VIBRATE)) {
@@ -147,6 +147,7 @@ public class ActionTarget {
             } else {
                 switchToNormalRingerMode();
             }
+
             return true;
         } else if (action.equals(ACTION_SILENT)) {
             if (mAm.getRingerMode() != AudioManager.RINGER_MODE_SILENT) {
@@ -154,6 +155,7 @@ public class ActionTarget {
             } else {
                 switchToNormalRingerMode();
             }
+
             return true;
         } else if (action.equals(ACTION_RING_SILENT_VIBRATE)) {
             int ringerMode = mAm.getRingerMode();
@@ -164,13 +166,15 @@ public class ActionTarget {
             } else {
                 switchToNormalRingerMode();
             }
+
             return true;
         } else if (action.equals(ACTION_NOTIFICATIONS)) {
             try {
                 getStatusBarService().expandNotificationsPanel();
             } catch (RemoteException e) {
-                // ignored
+                // Do nothing here
             }
+
             return true;
         } else if (action.equals(ACTION_TORCH)) {
             Intent intent = new Intent(TorchConstants.ACTION_TOGGLE_STATE);
@@ -188,6 +192,7 @@ public class ActionTarget {
             } catch (ActivityNotFoundException e) {
                 Log.e(TAG, "ActivityNotFound: [" + action + "]");
             }
+
             return false;
         }
     }
@@ -196,7 +201,7 @@ public class ActionTarget {
         try {
             ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
         } catch (RemoteException e) {
-            // ignored
+            // Do nothing here
         }
     }
 
@@ -229,7 +234,9 @@ public class ActionTarget {
         mHandler.removeCallbacks(mInjectKeyDownRunnable);
         mHandler.removeCallbacks(mInjectKeyUpRunnable);
         mHandler.post(mInjectKeyDownRunnable);
-        mHandler.postDelayed(mInjectKeyUpRunnable,10); // introduce small delay to handle key press
+
+        // Introduce small delay to handle key press
+        mHandler.postDelayed(mInjectKeyUpRunnable, 10);
     }
 
     final Runnable mInjectKeyDownRunnable = new Runnable() {
@@ -238,7 +245,8 @@ public class ActionTarget {
                     KeyEvent.ACTION_DOWN, mInjectKeyCode, 0, 0, KeyCharacterMap.VIRTUAL_KEYBOARD, 0,
                     KeyEvent.FLAG_FROM_SYSTEM | KeyEvent.FLAG_VIRTUAL_HARD_KEY,
                     InputDevice.SOURCE_KEYBOARD);
-            InputManager.getInstance().injectInputEvent(ev, InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
+            InputManager.getInstance().injectInputEvent(
+                    ev, InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
         }
     };
 
@@ -248,7 +256,8 @@ public class ActionTarget {
                     KeyEvent.ACTION_UP, mInjectKeyCode, 0, 0, KeyCharacterMap.VIRTUAL_KEYBOARD, 0,
                     KeyEvent.FLAG_FROM_SYSTEM | KeyEvent.FLAG_VIRTUAL_HARD_KEY,
                     InputDevice.SOURCE_KEYBOARD);
-            InputManager.getInstance().injectInputEvent(ev, InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
+            InputManager.getInstance().injectInputEvent(
+                    ev, InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
         }
     };
 
@@ -266,7 +275,7 @@ public class ActionTarget {
             if (res.activityInfo != null && !res.activityInfo.packageName.equals("android")) {
                 homePackage = res.activityInfo.packageName;
             } else {
-                // use default launcher package if we couldn't resolve it
+                // Use default launcher package if we couldn't resolve it
                 homePackage = "com.android.launcher";
             }
 
@@ -297,6 +306,7 @@ public class ActionTarget {
             if (mScreenshotConnection != null) {
                 return;
             }
+
             Intent intent = new Intent(mContext, TakeScreenshotService.class);
             ServiceConnection conn = new ServiceConnection() {
                 @Override
@@ -305,6 +315,7 @@ public class ActionTarget {
                         if (mScreenshotConnection != this) {
                             return;
                         }
+
                         Messenger messenger = new Messenger(service);
                         Message msg = Message.obtain(null, 1);
                         final ServiceConnection myConn = this;
@@ -322,24 +333,26 @@ public class ActionTarget {
                         };
                         msg.replyTo = new Messenger(h);
                         msg.arg1 = msg.arg2 = 0;
-                        // wait for the dialog box to close
+
+                        // Wait for the dialog box to close
                         try {
                             Thread.sleep(1000);
                         } catch (InterruptedException ie) {
-                        // Do nothing here
+                            // Do nothing here
                         }
 
-                        // take the screenshot
+                        // Take the screenshot
                         try {
                             messenger.send(msg);
                         } catch (RemoteException e) {
-                        // Do nothing here
+                            // Do nothing here
                         }
                     }
                 }
 
                 @Override
                 public void onServiceDisconnected(ComponentName name) {
+                    // Do nothing here
                 }
             };
 
