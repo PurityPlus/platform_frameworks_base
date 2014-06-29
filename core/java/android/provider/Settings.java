TheKang/frameworks_base

Slim Recents: Option to show topmost task [1/2]
Change-Id: Ib39d9b87b0fd30b4d34d4292af39e891038f337a

core/java/android/provider/Settings.java
@@ -4055,6 +4055,13 @@ public final class Settings {
         public static final String CUSTOM_RECENT = "custom_recent";
 
         /**
+         * Custom Recents: Show topmost task
+         *
+         * @hide
+         */
+        public static final String CUSTOM_RECENT_SHOW_TOPMOST = "custom_recent_show_topmost";
+
+        /**
          * Whether to show the network stats in the status bar
          * @hide
          */

packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
@@ -630,6 +630,12 @@ public class RecentPanelView {
         int firstItems = 0;
         final int firstExpandedItems =
                 mContext.getResources().getInteger(R.integer.expanded_items_default);
+
+        // Also show the topmost task?
+        boolean showTopmost = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.CUSTOM_RECENT_SHOW_TOPMOST, 0,
+                UserHandle.USER_CURRENT) == 1;
+
         boolean loadOneExcluded = true;
         // Get current task list. We do not need to do it in background. We only load MAX_TASKS.
         for (int i = 0, index = 0; i < numTasks && (index < MAX_TASKS); ++i) {
@@ -673,9 +679,10 @@ public class RecentPanelView {
                 }
 
                 if (i == 0) {
-                    // Skip the first task for our list but save it for later use.
+                    // Save the first task for later use.
                     mFirstTask = item;
-                } else {
+                }
+                if (i > 0 || showTopmost) {
                     // FirstExpandedItems value forces to show always the app screenshot
                     // if the old state is not known and the user has set expanded mode to auto.
                     // On all other items we check if they were expanded from the user

PurityPlus/platform_frameworks_base

core/java/android/provider/Settings.java
@@ -3261,6 +3261,13 @@ public final class Settings {
         public static final String CALL_UI_IN_BACKGROUND = "call_ui_in_background";
 
         /**
+         * Recent panel: Show topmost task
+         *
+         * @hide
+         */
+        public static final String RECENT_PANEL_SHOW_TOPMOST = "recent_panel_show_topmost";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
@@ -651,6 +651,12 @@ public class RecentPanelView {
         int firstItems = 0;
         final int firstExpandedItems =
                 mContext.getResources().getInteger(R.integer.expanded_items_default);
+
+        // Also show the topmost task?
+        boolean showTopmost = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.RECENT_PANEL_SHOW_TOPMOST, 0,
+                UserHandle.USER_CURRENT) == 1;
+
         boolean loadOneExcluded = true;
         // Get current task list. We do not need to do it in background. We only load MAX_TASKS.
         for (int i = 0, index = 0; i < numTasks && (index < MAX_TASKS); ++i) {
@@ -694,9 +700,10 @@ public class RecentPanelView {
                 }
 
                 if (i == 0) {
-                    // Skip the first task for our list but save it for later use.
+                    // Save the first task for later use.
                     mFirstTask = item;
-                } else {
+                }
+                if (i > 0 || showTopmost) {
                     // FirstExpandedItems value forces to show always the app screenshot
                     // if the old state is not known and the user has set expanded mode to auto.
                     // On all other items we check if they were expanded from the user