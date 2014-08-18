toyr99/android_frameworks_base

Revert "[1/2] Base: Network traffic indicator"
also

9942dfb Revert "Network stats custom colors (1/2)"
5e15cd4 Revert "Network stats: Hide if there's no traffic (1/2)"
This reverts commit eb8763c3daf90742fa022c24597e14e87be2ff8f.

Conflicts:
	core/java/android/provider/Settings.java
	packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkStatsView.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -33,7 +33,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final float mIconAlphaWhenOpaque;
 
     private View mLeftSide, mStatusIcons, mSignalCluster;
-    private View mBattery, mCircleBattery, mClock, mCenterClock, mTraffic, mStatsUp, mStatsDown;
+    private View mBattery, mCircleBattery, mClock, mCenterClock, mTraffic;
     private Animator mCurrentAnimation;
 
     public PhoneStatusBarTransitions(PhoneStatusBarView view) {
@@ -51,8 +51,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mCircleBattery = mView.findViewById(R.id.circle_battery);
         mCenterClock = mView.findViewById(R.id.center_clock);
         mClock = mView.findViewById(R.id.clock);
-        mStatsUp = mView.findViewById(R.id.bytes_tx);
-        mStatsDown = mView.findViewById(R.id.bytes_rx);
+        mTraffic = mView.findViewById(R.id.traffic);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -95,8 +94,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mLeftSide, newAlpha),
                     animateTransitionTo(mStatusIcons, newAlpha),
                     animateTransitionTo(mSignalCluster, newAlpha),
-                    animateTransitionTo(mStatsUp, newAlpha),
-                    animateTransitionTo(mStatsDown, newAlpha),
+                    animateTransitionTo(mTraffic, newAlpha),
                     animateTransitionTo(mBattery, newAlphaBC),
                     animateTransitionTo(mCircleBattery, newAlphaBC),
                     animateTransitionTo(mClock, newAlphaBC),
@@ -111,8 +109,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mLeftSide.setAlpha(newAlpha);
             mStatusIcons.setAlpha(newAlpha);
             mSignalCluster.setAlpha(newAlpha);
-            mStatsUp.setAlpha(newAlpha);
-            mStatsDown.setAlpha(newAlpha);
+            mTraffic.setAlpha(newAlpha);
             mBattery.setAlpha(newAlphaBC);
             mCircleBattery.setAlpha(newAlphaBC);
             mClock.setAlpha(newAlphaBC);

PurityPlus/platform_frameworks_base

	packages/SystemUI/res/values/colors.xml
	packages/SystemUI/res/values/mahdi_dimens.xml
	packages/SystemUI/src/com/android/systemui/quicksettings/NetworkSpeedTile.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -33,7 +33,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final float mIconAlphaWhenOpaque;
 
     private View mLeftSide, mStatusIcons, mSignalCluster;
-    private View mBattery, mDockBattery, mClock, mCenterClock, mTraffic, mStatsUp, mStatsDown;
+    private View mBattery, mDockBattery, mClock, mCenterClock, mTraffic;
     private Animator mCurrentAnimation;
 
     public PhoneStatusBarTransitions(PhoneStatusBarView view) {
@@ -51,8 +51,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mDockBattery = mView.findViewById(R.id.dock_battery);
         mCenterClock = mView.findViewById(R.id.center_clock);
         mClock = mView.findViewById(R.id.clock);
-        mStatsUp = mView.findViewById(R.id.bytes_tx);
-        mStatsDown = mView.findViewById(R.id.bytes_rx);
+        mTraffic = mView.findViewById(R.id.traffic);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -96,8 +95,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mStatusIcons, newAlpha),
                     animateTransitionTo(mSignalCluster, newAlpha),
                     animateTransitionTo(mDockBattery, newAlphaBC),
-                    animateTransitionTo(mStatsUp, newAlpha),
-                    animateTransitionTo(mStatsDown, newAlpha),
+                    animateTransitionTo(mTraffic, newAlpha),
                     animateTransitionTo(mBattery, newAlphaBC),
                     animateTransitionTo(mClock, newAlphaBC),
                     animateTransitionTo(mCenterClock, newAlphaBC)
@@ -112,8 +110,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mStatusIcons.setAlpha(newAlpha);
             mSignalCluster.setAlpha(newAlpha);
             mDockBattery.setAlpha(newAlphaBC);
-            mStatsUp.setAlpha(newAlpha);
-            mStatsDown.setAlpha(newAlpha);
+            mTraffic.setAlpha(newAlpha);
             mBattery.setAlpha(newAlphaBC);
             mClock.setAlpha(newAlphaBC);
             mCenterClock.setAlpha(newAlphaBC);